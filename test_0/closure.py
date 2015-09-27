cdacf4ffe0b52cd8f53c52075982064294ed5a0a
youzipi <blake_12@qq.com>
2015-09-27 19:34:23 +0800

    update

--- test_0/closure.py
+++ test_0/closure.py
@@ -1,1 +1,10 @@
 __author__ = 'youzipi'
+
+def make_adder(addend):
+    def adder(augend):
+        return augend + addend
+    return adder
+p = make_adder(23)
+q = make_adder(44)
+print p(100)
+print q(100)
