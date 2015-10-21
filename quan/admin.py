5470bdf4f68743d328b5fc21d37110513589c67b
youzipi <blake_12@qq.com>
2015-10-21 21:32:16 +0800

    modify Article add filed:category,tags

--- quan/admin.py
+++ quan/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from quan.models import Article, User
 
-admin.site.register(User)
-admin.site.register(Article)
+
 # admin.site.register(MarkdownModelAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['content', 'category', 'tags']
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(User)
