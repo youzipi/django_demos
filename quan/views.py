443029201f032281022e10108cf54484a8af56fe
youzipi <blake_12@qq.com>
2015-10-22 12:40:52 +0800

    add dashbroad

--- quan/views.py
+++ quan/views.py
@@ -1,12 +1,24 @@
+# coding=utf-8
 import datetime
+import logging
 
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView, View, ListView
 
 from quan.forms import ArticleForm
-from quan.models import Article
+from quan.models import Article  # , PythonPost
+
+
+# 缓存
+# try:
+#    cache = caches['memcache']
+# except ImportError as e:
+#    cache = caches['default']
+
+# logger
+logger = logging.getLogger(__name__)
 
 
 def index(request):
@@ -20,10 +32,31 @@ def index(request):
 #     _post = get_object_or_404(Article, pk=post_id)
 #     return render(request, 'quan/post.html', {'post': _post})
 
+class BaseMixin(object):
+    def get_context_data(self, *args, **kwargs):
+        context = super(BaseMixin, self).get_context_data(**kwargs)
+        try:
+            # 热门文章
+            context['hot_article_list'] = Article.objects.order_by("-view_times")[0:10]
+            # # 导航条
+            # context['nav_list'] = Nav.objects.filter(status=0)
+            # # 最新评论
+            # context['latest_comment_list'] = Comment.objects.order_by("-create_time")[0:10]
+
+        except Exception as e:
+            logger.error(u'[BaseMixin]加载基本信息出错')
+
+        return context
+
+
 class ArticleView(View):
     def get(self, request, post_id):
         _post = get_object_or_404(Article, pk=post_id)
         return render(request, 'quan/post.html', {'article': _post})
+
+
+class DashBroadView(ListView):
+    model = Article
 
 
 class ArticlesView(View):
